7d688a5a239e99a1a8dcb69c20d6a7e9463f65fd
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-07-20 18:12:18 -0300

    Initial commit

---
+++ aula01/ex01/ex1.js
@@ -0,0 +1,10 @@
+const mostrarSite = true
+const site = 'https://blueedtech.com.br/'
+
+console.log('Hello Node.js')
+console.log('Meu nome é Paulo')
+console.log('E eu estou aprendendo Node.js com a Blue')
+
+if (mostrarSite) {
+  console.log(site)
+}
