4c83f93b686049f896b7a80ea43d6a1f23acbe7f
PAULO HENRIQUE GAMA <paulohg_pool@hotmail.com>
2021-08-21 22:09:17 -0300

    Projeto 2 finalizado

--- v2/node/aula02/proj02/index.js
+++ v2/node/aula02/proj02/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-
 app = express();
 
 const port = 3000;
@@ -11,64 +10,68 @@ const games = [
   'Eudemons',
   'Asa de Cristal',
   'Ragnarok',
-  'Perfect World'
-
+  'Perfect World',
 ];
 
-
-
-const escolha = (min,max) => {
+const escolha = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
-}
+};
 
 app.get('/', function (req, res) {
   res.send('Pagina de Games');
-})
+});
 
 app.get('/games', function (req, res) {
   res.send(games);
-})
+});
 
-console.log(escolha(0,4))
+console.log(escolha(0, 4));
 app.get('/jogar', function (req, res) {
-  res.send(games[escolha(0,games.length)])
-})
+  res.send(games[escolha(0, games.length)]);
+});
 
 app.get('/games/:id', function (req, res) {
-  const id = req.params.id-1;
+  const id = req.params.id - 1;
 
   const game = games[id];
-  if(games[id]){
-  res.send(game);
-  }else{
-    res.send("Não existe esse jogo na lista.")
-  }  
-})
+  if (games[id]) {
+    res.send(game);
+  } else {
+    res.send('Não existe esse jogo na lista.');
+  }
+});
 
-app.post("/games", (req,res) => {
+app.post('/games', (req, res) => {
   const game = req.body.game;
-  
+
   games.push(game);
 
-  res.send(`O jogo ${game} foi adicionado com sucesso!`); 
-})
+  res.send(`O jogo ${game} foi adicionado com sucesso!`);
+});
 
-app.put('/games/:id',(req, res)=>{
-  const id = req.params.id-1;
-  let gameAnt = games[id]
-  
-  if(!games[id]){
-    res.status(404).send({message:"O jogo não encontrado!"})
+app.put('/games/:id', (req, res) => {
+  const id = req.params.id - 1;
+  let gameAnt = games[id];
+
+  if (!games[id]) {
+    res.status(404).send({ message: 'O jogo não encontrado!' });
   }
   games[id] = req.body.game;
-  res.send(`O jogo ${gameAnt} foi subistituido pelo jogo ${games[id]}`); 
-
-})
+  res.send(`O jogo ${gameAnt} foi subistituido pelo jogo ${games[id]}`);
+});
 
 app.delete('/games/:id', (req, res) => {
-  
-})
+  const id = req.params.id - 1;
+
+  if (!games[id]) {
+    res.status(404).send({ message: 'O jogo não encontrencontrado!' });
+  }
+
+  games.splice(id, 1);
+
+  res.send(`Jogo excluido com sucesso!!`);
+});
 
 app.listen(port, () => {
-  console.info(`App rodando em http://localhost:${port}`)
-})
+  console.info(`App rodando em http://localhost:${port}`);
+});
